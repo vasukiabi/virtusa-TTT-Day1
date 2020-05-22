a7aad59e0be3b09dc521b3738ab47c018c5939cf
vasukiabi <41136325+vasukiabi@users.noreply.github.com>
2020-05-22 07:05:20 +0530

    Add files via upload

---
+++ CalculatorApp/src/test/java/virtusa/ttt/com/CalculatorApp/CalculatorTest.java
@@ -0,0 +1,20 @@
+package virtusa.ttt.com.CalculatorApp;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CalculatorTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
