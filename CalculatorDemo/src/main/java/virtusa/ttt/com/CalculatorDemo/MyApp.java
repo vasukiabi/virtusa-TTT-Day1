887d2c06994af3b12ad9147a5589383610aaa499
vasukiabi <41136325+vasukiabi@users.noreply.github.com>
2020-05-22 07:06:16 +0530

    Add files via upload

---
+++ CalculatorDemo/src/main/java/virtusa/ttt/com/CalculatorDemo/MyApp.java
@@ -0,0 +1,17 @@
+package virtusa.ttt.com.CalculatorDemo;
+
+import virtusa.ttt.com.CalculatorApp.Calculator;
+
+/**
+ * Hello world!
+ *
+ */
+public class MyApp 
+{
+    public static void main( String[] args )
+    {
+    	Calculator c = new Calculator();
+    	c.calculateinterest();
+    			
+    }
+}
---
+++ CalculatorDemo/src/test/java/virtusa/ttt/com/CalculatorDemo/MyAppTest.java
@@ -0,0 +1,20 @@
+package virtusa.ttt.com.CalculatorDemo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class MyAppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
